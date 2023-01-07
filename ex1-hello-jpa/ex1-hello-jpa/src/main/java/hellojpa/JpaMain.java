bf34d1f27724224b7ce20c28db353750d79320d1
ryeohwan <arh951019@gmail.com>
2023-01-07 15:15:53 +0900

    code: add mapping

--- ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,18 +13,12 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
 
-            // 영속성 컨텍스트 통으로 완전 다 날려버려(1차 캐시)
-            em.clear();
-            // 얘 이후 다시 조회하면 영속성에 없어서 다시 1차 캐시로 올려
-
-            Member member2 = em.find(Member.class, 150L);
-
-            // 그럼 얘는 Jpa에서 관리 안해. 그래서 커밋할 때 반영 안됨
-            System.out.println("======================");
+            Member member = new Member();
+            member.setId(1L);
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
+            em.persist(member);
             tx.commit();
 
 
--- ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,37 +1,82 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
+import javax.persistence.*;
+import java.util.Date;
 @Entity
 public class Member {
-    // jpa 는 동적으로 생성해내야 한다. 기본 생성자가 없으면 빨간 줄 뜬다.
-
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name")  // db 컬럼명은 name으로 들어가는 것
+    private String username;
 
-    public Member (Long id, String name){
-        this.id = id;
-        this.name = name;
-    }
+
+    private Integer age;
+    @Enumerated(EnumType.STRING)  // 자바 enum타입 근데 db는 enum이 없다.
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)  // 날짜 타입
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob  // varchar 넘는 큰 값이 오면 (문자면 clob )
+    private String description;
 
     public Member(){
 
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
